e917c3f449c0ba8fcbc289e72a1d7c66dd8822d4
suearez_home_linux <cubin4ik@mail.ru>
2022-06-06 02:21:08 +0300

    votes tabs are added to user admin profile

--- account/admin.py
+++ account/admin.py
@@ -1,4 +1,16 @@
 from django.contrib import admin
 from .models import User
+from votes.models import Votes, Nominations
 
-admin.site.register(User)
+
+class VotesInline(admin.TabularInline):
+    model = Votes
+    extra = 0
+    fk_name = 'voter'
+
+
+class UserAdmin(admin.ModelAdmin):
+    inlines = [VotesInline]
+
+
+admin.site.register(User, UserAdmin)
